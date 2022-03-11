a6fbddc68d12c042398110b437398565da5710b5
childmike22 <58493509+childmike22@users.noreply.github.com>
2022-03-10 23:18:28 -0800

    Create transform.py

---
+++ ETL Projects/RealEstateData/python/transform.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+
+# TODO 1: Clean the data - replace any NaN values
+# for column in df.columns:
+#     print(f"{column} nan count: {df[column].isna().sum()}")
+
+# Only column with nan values is the postal code column - lets replace it with 'not specified'
+
+
+def make_lowercase(df):
+    cols_to_lower = ['Address', 'Description of Property', 'Postal Code', 'County']
+    for col in cols_to_lower:
+        df[col] = df[col].str.lower()
+    return df
+
+
+def fill_na_values(df):
+    df['Postal Code'] = df['Postal Code'].fillna('not specified')
+    return df
+
+# TODO 1A: Clean the data - change the date format of our sale date column
+
+
+def convert_dates_to_correct_formats(df):
+    df['Date_of_Sale_(mm/dd/yyyy)'] = pd.to_datetime(df['Date of Sale (dd/mm/yyyy)'], format='%d/%m/%Y').dt.strftime('%m-%d-%Y')
+    return df
+
+# TODO 1B: Clean the data - convert our price column to a float
+
+
+def convert_price_to_int(df):
+    df['Price ()'] = df['Price ()'].str.replace('', '')
+    df['Price ()'] = df['Price ()'].str.replace(',', '')
+    df['Price ()'] = df['Price ()'].astype(float)
+    df['Price ()'] = df['Price ()'].astype(int)
+    return df
+
+
+# TODO 1C: Clean the data - simplofy the property description field to just second-hand or new
+def group_property_type(df):
+    df['Property Type'] = np.where(df['Description of Property'].str.contains('new'), 'new', 'second hand')
+    return df
+
+
+def fully_transform_df(df):
+    make_lowercase(df)
+    fill_na_values(df)
+    convert_price_to_int(df)
+    group_property_type(df)
+    convert_dates_to_correct_formats(df)
+    return df
