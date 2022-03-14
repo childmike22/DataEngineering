1baa2e6653129b99cb06f02cd10bbd1f170854ae
childmike22 <58493509+childmike22@users.noreply.github.com>
2022-03-14 12:56:05 -0700

    Update main.py

--- ETL Projects/RealEstateData/python/main.py
+++ ETL Projects/RealEstateData/python/main.py
@@ -1,10 +1,12 @@
 from extract import extract_data
 from transform import fully_transform_df
+from load import load_all_data
 
 
 if __name__ == '__main__':
     # EXTRACT
-    trial = extract_data()
+    extracted_data = extract_data()
     # TRANSFORM
-    clean = fully_transform_df(trial)
+    data_to_load = fully_transform_df(extracted_data)
     # LOAD
+    load_all_data(data_to_load)
